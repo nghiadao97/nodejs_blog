95b8be2c7d1a4d61d21f8c5fcfeeaf6371b51fa4
nghiadao97 <58335461+nghiadao97@users.noreply.github.com>
2021-06-09 17:46:53 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -10,24 +10,29 @@ const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
-app.use(express.urlencoded({
-  extended: true,
-})); // Gửi theo dạng form HTML
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+); // Gửi theo dạng form HTML
 app.use(express.json()); // Gửi từ code javascript lên server
 
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource\\views'));
 
 // Routes init
 route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+  app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+  });
